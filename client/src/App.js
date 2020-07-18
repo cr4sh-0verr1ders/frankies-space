0e02071f0eaa4b20f4c42ca86352014d06419553
ralismark <timmy.yao@outlook.com.au>
2020-07-18 14:42:59 +1000

    Add User class

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './map.jpg';
 import './App.css';
+import { User } from './User';
 
 function Map(props) {
   const styles = {
     "--x": props.x,
     "--y": props.y,
   };
+
   return (
     <img className="map" style={styles} src={logo}/>
   );
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Map x={0} y={0} />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      self: new User(),
+    }
+  }
+
+  render() {
+    const self = this.state.self;
+    return (
+      <div className="App">
+        <Map x={self.x} y={self.y} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/User.js
@@ -0,0 +1,12 @@
+class User {
+  constructor() {
+    this.setXY(0, 0);
+  }
+
+  setXY(newX, newY) {
+    this.x = newX;
+    this.y = newY;
+  }
+}
+
+export { User };
