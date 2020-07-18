53350d04ae998ee7aaaf839682a3970bae918fdd
ralismark <timmy.yao@outlook.com.au>
2020-07-18 14:24:52 +1000

    Add map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './map.jpg';
 import './App.css';
+
+function Map(props) {
+  const styles = {
+    "--x": props.x,
+    "--y": props.y,
+  };
+  return (
+    <img className="map" style={styles} src={logo}/>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map x={0} y={0} />
     </div>
   );
 }
